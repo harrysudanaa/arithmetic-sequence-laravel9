016410e5cb2dd9cedd2a80376f86637e9b31e47b
harrysudanaa <komangharry02@gmail.com>
2022-11-20 15:28:27 +0800

    Initial commit

---
+++ app/Http/Controllers/NumberController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NumberController extends Controller
+{
+    public function index()
+    {
+        return view('frontpage.home');
+    }
+    public function store(Request $request)
+    {
+        $input = $request->except("_token");
+        // dd($input);
+        // a = first term, b = difference between term, n = term of n
+        $a = $input["a"];
+        $b = $input["b"];
+        $n = $input["n"];
+        $tmp = $a + $b;
+        $result = [$a];
+        $un = 0;
+        for ($i = 1; $i < $n; $i++) {
+            $un = $a + ($n - 1) * $b;
+            $result[$i] = $tmp;
+            $tmp = $tmp + $b;
+        }
+        // dd($limit);
+        return view('frontpage.result', compact(['result']));
+    }
+}
---
+++ resources/views/frontpage/home.blade.php
@@ -0,0 +1,24 @@
+<x-home-layout>
+  <div class="flex flex-col items-center justify-center">
+    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8">
+      <form class="space-y-6" action="/store" method="POST">
+        @csrf
+        <h5 class="text-xl font-bold text-gray-900">Arithmetic Sequence Generator</h5>
+        <span class="text-slate-400 text-xs">Calculator for Arithmetic Sequence</span>
+        <div>
+          <label for="a" class="block mb-2 text-sm font-medium text-gray-900">First Term</label>
+          <input type="text" name="a" id="a" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Enter first term" required>
+        </div>
+        <div>
+          <label for="b" class="block mb-2 text-sm font-medium text-gray-900">Difference Between Terms</label>
+          <input type="text" name="b" id="b" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Enter differences between terms" required>
+        </div>
+        <div>
+          <label for="n" class="block mb-2 text-sm font-medium text-gray-900">Term of N</label>
+          <input type="text" name="n" id="n" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Enter term of n" required>
+        </div>
+        <button type="submit" class="w-24 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Compute</button>
+      </form>
+    </div>
+  </div>
+</x-home-layout>
